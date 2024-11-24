cff55f12194db1a3768bbbb8a441a1ea050e6ec7
tiagosoussa7 <tg.sousa77.t@gmail.com>
2024-11-24 09:16:17 -0300

    style: configuração tailwind, css global e fontes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      sm: "960px",
+      sm: "1200px",
+    },
+    fontFamily: {
+      primary: "var(--font-jetBrains-Mono)",
+      secondary: "var(--font-bebas-Neue)",
+    },
+    extend: {
+      colors: {
+        primary: "#141416",
+        secondary: "#1c1c22",
+        accent: {
+          DEFAULT: "#9400f9",
+          hover: "#ba5ff9",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
